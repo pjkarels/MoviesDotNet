8e84149cde07dd9f418a31892ca7afa264f97811
Paul Karels <pkarels@nerdery.com>
2019-06-06 21:46:35 -0500

    Add add movie icon and page

---
+++ Movies/Pages/Movies/Add.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movies.Pages.Movies
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
